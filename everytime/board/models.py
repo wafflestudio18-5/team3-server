3280dd8b2b5a359b3653190de14ab617aecd6cf6
tictactoeid <jungjimin424@naver.com>
2020-12-23 15:24:39 +0900

    board, post models 작성 / views 작성중

--- everytime/board/models.py
+++ everytime/board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(blank = False, max_length = 150)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    post_title = models.BooleanField(null = False)
--- everytime/board/views.py
+++ everytime/board/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+
+from board.models import Board
 
 # Create your views here.
+
+class BoardViewSet(viewsets.GenericViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = (IsAuthenticated(),)
+
+    def list(self, request): # GET /find/board/list: List information about boards.
+        board = Board.objects.all().order_by('created_at') # cache?
+        data = self.get_serializer(board, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
