0f9f50e2490b9fe5543001e492dd73dd3e279f34
GGlifer <abyss7500@snu.ac.kr>
2021-01-09 09:44:35 +0900

    CORS / www

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -190,5 +190,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://3.17.37.198',
     'http://waverytime.shop',
     'https://waverytime.shop'
+    'http://www.waverytime.shop',
+    'https://www.waverytime.shop',
 ]
 CORS_ALLOW_CREDENTIALS = True
